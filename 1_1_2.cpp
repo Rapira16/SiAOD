ab2543e8023d26ca9d62b4d66f8d8ecae00b098c
Sukhanov Dmitry <79788948+Rapira16@users.noreply.github.com>
2024-02-20 16:34:15 +0300

    Update 1_1_2.cpp

--- 1_1_2.cpp
+++ 1_1_2.cpp
@@ -1,61 +1,106 @@
 #include <iostream>
 using namespace std;
 
-int** randomMatrix(int row, int column){
+void printArray(int* array, int length) {
+    cout << "array = ";
+    for (int i = 0; i < length; i++) {
+        cout << array[i] << " ";
+    }
+    cout << "\nlength = " << length << endl;
+}
+
+int* randomNumberArray(int size) {
+    int* array = new int[size];
     srand(time(NULL));
 
-    int** matrix = new int *[row];
-
-    for (int k = 0; k < row; k++){
-        matrix[k] = new int[column];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand() % 5;
     }
+    return array;
+}
 
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < column; j++) {
-            matrix[i][j] = rand()%10;
-            cout << matrix[i][j] << " ";
+void delFirstMethod(int* x, int n, int key) {
+    int count = 0;
+    int i = 0;
+    count++;
+    while (i < n) {
+        count++;
+        if (x[i] == key) {
+            count++;
+            for (int j = i; j < n - 1; j++) {
+                count += 1;
+                x[j] = x[j + 1];
+                count++;
+            }
+            count++;
+            n = n - 1;
+            count++;
         }
-        cout << "\n";
-    }
-    return matrix;
-}
-
-int* randomVector(int height){
-
-    int* vector = new int[height];
-
-    for (int i = 0; i < height; ++i) {
-        vector[i] = rand()%10;
-        cout << vector[i] << "\n";
-    }
-    return vector;
-}
-
-void printMatrix(int* Matrix, int height){
-    for (int i = 0; i < height; ++i) {
-        cout << Matrix[i] << "\n";
-    }
-}
-
-void multiply(int** matrix, int* vector, int height, int width){
-    int* result = new int[height];
-    for (int i = 0; i < height; ++i) {
-        int summ_row = 0;
-        for (int j = 0; j < width; ++j) {
-            summ_row += matrix[i][j]*vector[j];
+        else {
+            count++;
+            i = i + 1;
+            count++;
         }
-        result[i] = summ_row;
     }
+    count++;
 
-    printMatrix(result, height);
+    printArray(x, n);
+    cout << "all operations = " << count << "\n";
+}
+
+void delOtherMethod(int* x, int n, int key) {
+    int count = 0;
+    int j = 0;
+    count++;
+    for (int i = 0; i < n; i++) {
+        count++;
+        x[j] = x[i];
+        count++;
+        if (x[i] != key) {
+            j++;
+            count++;
+        }
+        count++;
+    }
+    count++;
+    n = j;
+    count++;
+
+    printArray(x, n);
+    cout << "all operations = " << count << "\n";
 
 }
 
 int main() {
-    int height = 3, width = 2;
-    int** matrix = randomMatrix(height, width);
-    int* vector = randomVector(width);
-    multiply(matrix, vector, height, width);
+    int size_1 = 10;
+    int key_1 = 3;
+    int* array_1_random = new int[size_1];
 
+    array_1_random = randomNumberArray(size_1);
+    int* array_best = new int[10]{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+    int* array_worst = new int[10]{ 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+
+    cout << "delFirstMethod: \n";
+    cout << "random ";
+    delFirstMethod(array_1_random, size_1, key_1);
+    cout << "\n";
+    cout << "best ";
+    delFirstMethod(array_best, 10, key_1);
+    cout << "\n";
+    cout << "worst ";
+    delFirstMethod(array_worst, 10, key_1);
+    cout << "\ndelOtherMethod: \n";
+    cout << "\n";
+    cout << "random ";
+    delOtherMethod(array_1_random, size_1, key_1);
+    cout << "\n";
+    cout << "worst ";
+    delOtherMethod(array_best, 10, key_1);
+    cout << "\n";
+    cout << "best ";
+    delOtherMethod(array_worst, 10, key_1);
+
+    delete[] array_1_random;
     return 0;
 }
