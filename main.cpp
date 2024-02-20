bb33b6abfdd38556eafbbeb58a467a7db99e47e2
Sukhanov Dmitry <79788948+Rapira16@users.noreply.github.com>
2024-02-20 14:31:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ void printArray(int *array, int length){
     cout << "\n" << length << endl;
 }
 
-int *randomNumberArray(int &size){
+int *randomNumberArray(int size){
     int *array = new int[size];
     srand(time(NULL));
 
@@ -52,18 +52,14 @@ void delOtherMethod(int *x, int n, int key){
 
 int main() {
     int size_1 = 10;
-    int size_2 = 100;
     int key_1 = 3;
-    int key_2 = 4;
-
-    int *array_1_random = randomNumberArray(size_1);
-    int *array_2_random = randomNumberArray(size_2);
-    int *array_best = new int[10]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-    int *array_worst = new int[10]{3, 3, 3, 3, 3, 3 ,3, 3, 3, 3};
+    int* array_1_random = new int[size_1];
+    
+    array_1_random = randomNumberArray(size_1);
+    int* array_best = new int[10]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    int* array_worst = new int[10]{3, 3, 3, 3, 3, 3 ,3, 3, 3, 3};
 
     delFirstMethod(array_1_random, size_1, key_1);
-    cout << "\n";
-    delFirstMethod(array_2_random, size_2, key_2);
     cout << "\n";
     delFirstMethod(array_best, 10, key_1);
     cout << "\n";
@@ -71,13 +67,10 @@ int main() {
     cout << "\n";
     delOtherMethod(array_1_random, size_1, key_1);
     cout << "\n";
-    delOtherMethod(array_2_random, size_2, key_2);
-    cout << "\n";
     delOtherMethod(array_best, 10, key_1);
     cout << "\n";
     delOtherMethod(array_worst, 10, key_1);
 
     delete[] array_1_random;
-    delete[] array_2_random;
     return 0;
 }
